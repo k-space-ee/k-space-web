f4509f9f43c4964335d578031434c1f657ba4a93
teras <karlnitram@gmail.com>
2017-10-07 19:21:26 +0300

    Lauri index

--- challenges/settings.py
+++ challenges/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gze3mreum6caz26_k2zq8(zn+)v3pdfaup+-e20eu@vca5st=b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['k-space.ee']
+ALLOWED_HOSTS = ['k-space.ee', '127.0.0.1']
 
 
 # Application definition

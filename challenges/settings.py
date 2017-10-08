9378647a24131fc9bf35f36b610d8f7e52dd2f9d
teras <karlnitram@gmail.com>
2017-10-08 11:47:57 +0300

    Moved static files

--- challenges/settings.py
+++ challenges/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 MEDIA_URL = '/media/'
